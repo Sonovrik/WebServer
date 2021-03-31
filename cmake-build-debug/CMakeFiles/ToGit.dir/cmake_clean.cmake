004f041ea00d424b263acbda48d938e99751e468
kmoaning <kmoaning@student.21-school.ru>
2021-03-31 18:12:11 +0300

    qqq

---
+++ cmake-build-debug/CMakeFiles/ToGit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToGit.dir/main.cpp.o"
+  "ToGit"
+  "ToGit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ToGit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ToGit.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ToGit.
+# This may be replaced when dependencies are built.
